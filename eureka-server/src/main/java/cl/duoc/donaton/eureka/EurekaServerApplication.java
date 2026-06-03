849dbf6bc4dae93afac3666f84d6f4ccbde9f073
BrvnoV <he.valenzuelaf@duocuc.cl>
2026-06-03 14:42:54 -0400

    feat: configurar e implementar servidor de descubrimiento Eureka en puerto 8761

--- eureka-server/src/main/java/cl/duoc/donaton/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/cl/duoc/donaton/eureka/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package cl.duoc.donaton.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // <-- Esta anotación transforma el servicio en un servidor de descubrimiento
 public class EurekaServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
