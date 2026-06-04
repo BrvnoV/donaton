7bc744cec700a0bff0b3251a10a92a23797a5d14
RodrigoG21 <168581982+RodrigoG21@users.noreply.github.com>
2026-06-03 23:07:14 -0400

    feat(bff): configurar dependencias de resiliencia, Eureka client y WebClient balanceado

--- donaton-bff/src/main/java/cl/duoc/donaton/bff/DonatonBffApplication.java
+++ donaton-bff/src/main/java/cl/duoc/donaton/bff/DonatonBffApplication.java
@@ -2,8 +2,10 @@ package cl.duoc.donaton.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // ◄ Conecta el BFF con el panel de Eureka para descubrir los microservicios por su nombre
 public class DonatonBffApplication {
 
 	public static void main(String[] args) {
